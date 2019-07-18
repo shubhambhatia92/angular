b5f7fe8fb5d6bc4511f4ecf0ad22eb5138b892ba
shubham bhatia <shubhambhatia92@gmail.com>
2019-07-19 01:49:38 +0530

    angular components 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
-
+import { from } from 'rxjs';
+import { MenuComponent } from './menu/menu.component';
+import 'hammerjs';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatListModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
